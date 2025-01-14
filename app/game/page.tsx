d2f0db3dd67b13c08db65ccc489bf4de5d53d573
nikjavor <nik.javor+git@proton.me>
2025-01-14 21:58:18 +0100

    Style game info part of game page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,17 +2,17 @@ function GameInfo() {
   return (
     <>
       <h1 className="text-4xl text-center">Game Name</h1>
-      <table className="border-2 w-full">
-        <tbody className="border-2 border-red-500">
-          <tr className="border-2 border-green-500 *:text-center">
-            <td className="border-2 border-blue-500">player1</td>
-            <td>player2</td>
-            <td>player3</td>
-            <td>player4</td>
+      <p className="text-center">2.12.2005</p>
+      <table className="w-full">
+        <tbody className="border-red-500">
+          <tr className="border-green-500 *:text-center *:text-xs">
+            <td>p1:leonabr</td>
+            <td>p2:nikjavo</td>
+            <td>p3:najajav</td>
+            <td>p4:crtstro</td>
           </tr>
         </tbody>
       </table>
-      <p className="text-center">2.12.2005</p>
     </>
   );
 }
