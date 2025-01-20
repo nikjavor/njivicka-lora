49d6259f8c3bbd6c34d2b1d516dbbb3d38cc7fe9
nikjavor <nik.javor+git@proton.me>
2025-01-20 14:36:30 +0100

    Add dark mode support to play game button

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-4xl">Lora score</h1>
-      <Link href={"/game?g=1"} className="p-3 bg-black dark:bg-white text-white text-3xl font-bold rounded-xl">Play game</Link>
+      <Link href={"/game?g=1"} className="p-3 bg-black dark:bg-white text-white dark:text-black text-3xl font-bold rounded-xl">Play game</Link>
     </div>
   );
 }
