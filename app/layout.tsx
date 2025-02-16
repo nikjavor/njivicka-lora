680002e9e627c9e5613a93e1529a42a993ed9767
nikjavor <nik.javor+git@proton.me>
2025-02-16 18:11:57 +0100

    Fix publishable key error

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={`${spaceMono.className} antialiased bg-background-color text-text-color`}>
           {children}
