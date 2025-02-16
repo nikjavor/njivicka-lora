4bab132ecc6c51cb512bf16d760d9df3b2e12399
nikjavor <nik.javor+git@proton.me>
2025-02-16 20:21:27 +0100

    Pls work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { spaceMono } from "@/app/ui/fonts";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-import './globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Njivička lora",
@@ -13,15 +11,17 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider publishableKey="pk_test_b3Blbi1zaHJpbXAtNjYuY2xlcmsuYWNjb3VudHMuZGV2JA">
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
       <html lang="en">
-        <body className={`${spaceMono.className} antialiased bg-background-color text-text-color`}>
+        <body
+          className={`${spaceMono.className} antialiased bg-background-color text-text-color`}
+        >
           {children}
-        </body >
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
